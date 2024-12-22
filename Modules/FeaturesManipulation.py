108656a4049bf865105eef71a1970eab23d76434
marcellinow <marcellino.gabriel12@gmail.com>
2024-12-22 14:01:10 +0700

    finishing feature manipulation

--- Modules/FeaturesManipulation.py
+++ Modules/FeaturesManipulation.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 import re
-import urllib.parse
+import urllib
+from urllib.parse import urlparse
 from unidecode import unidecode
-import Levenshtein
-import socket
 import tldextract
 
 class FeaturesManipulation:
@@ -16,10 +15,11 @@ class FeaturesManipulation:
         
         '''
         self.df = df
-
-        def features_manipulation(self):
-            self.df['TLDMajorLegit'] = self.df['TLD']
-
+        self.features_manipulation()
+    def features_manipulation(self):
+        self.df = column_major_legit(self.df, 'TLD', merge=True)
+        legit_domains = column_major_legit(self.df, 'Domain',merge=False)
+        self.df['HasObfuscation'] = self.df['URL'].apply(lambda x: has_obfuscation(x,legit_domains=legit_domains))
 
 def extract_domain(url):
     '''
@@ -29,6 +29,7 @@ def extract_domain(url):
     output:
         domain : str
     '''
+    # print(f"extract domain\n")
     if pd.isna(url):
         return np.nan
     extracted = tldextract.extract(url)
@@ -36,82 +37,101 @@ def extract_domain(url):
     # print(f'extracted domain : {extracted.domain} \n')
     return extracted.domain
 
-def get_tld_counts(df,tld,label='label'):
+def get_tld_counts(df, tld, label='label'):
     '''
-    Function to count how much the phising (label == 0) certain tld
+    Function to count how much the phishing (label == 0) certain TLD
     input:
-        df  : df
+        df  : DataFrame
         tld : str
-        label   : 'label'
+        label : str
     output:
-        count_0 : how much the label 0
-        count_1 : how much the label 1
+        count_0 : int
+        count_1 : int
     '''
-    count_0 = len(df[(df['TLD'] == tld) & (df[label] == 0)])
-    count_1 = len(df[(df['TLD'] == tld) & (df[label] == 1)])
+    if 'TLD' not in df.columns or label not in df.columns:
+        raise ValueError("DataFrame must contain 'TLD' and '{}' columns".format(label))
+
+    tld_counts = df[df['TLD'] == tld][label].value_counts()
+    count_0 = tld_counts.get(0, 0)
+    count_1 = tld_counts.get(1, 0)
+    
     return count_0, count_1
 
-def column_major_legit(df,column,label='label'):
+def column_major_legit(df, column, label='label', merge=False):
     '''
-    Function to check the majority of a certain column is legit or phising (based on amount of label).
-    If amount of label 0 > amount of label 1, then the column is majority phising
+    Function to check the majority of a certain column is legit or phishing (based on amount of label).
+    If amount of label 0 > amount of label 1, then the column is majority phishing.
     input:
-        df  : dataframe
-        column  : column name (only accept one column)
-        label:  'label'
+        df     : dataframe
+        column : column name (only accept one column)
+        label  : label column name
+        merge  : boolean, if True, merge the result back to the original dataframe
     output:
-        column  : with value 1 (majority legit), 0 (majority phising)
+        If merge is False, return list of columns name with majority legitimate link.
+        If merge is True, return the original dataframe with the new column added.
     '''
-    if len(column) > 1:
-        raise ValueError("Only one column name should be provided")
-    df.groupby(column)[label].value_counts().unstack(fill_value=0)
+    if column not in df.columns or label not in df.columns:
+        raise ValueError(f"DataFrame must contain '{column}' and '{label}' columns")
+    
+    print("entering algorithm to find majority legitimate columns")
     col_counts = df.groupby(column)[label].value_counts().unstack(fill_value=0)
     col_name = f'{column}MajorityLegit'
-    col_counts[col_name] = col_counts[0] > col_counts[1]
-    col_counts[col_name] = col_counts['TLDMajoritylegitimate'].map({True: 0, False: 1})
-    df = df.merge(col_counts[[col_name]],on=column,how='left')
-    return df
+    col_counts[col_name] = col_counts[0] < col_counts[1]
+    col_counts[col_name] = col_counts[col_name].map({True: 0, False: 1})
+    print("DONE\n")
+    
+    if merge:
+        df = df.merge(col_counts[[col_name]], on=column, how='left')
+        return df
+    else:
+        return col_counts[col_counts[col_name] == 0].index.to_list()
 
-def has_obfuscation(df,url):
+def has_obfuscation(url, legit_domains):
     '''
     Function to check whether the URL contains obfuscated method inside of it
     input:
-        df  : dataframe
         url : str
+        legit_domains : list of legitimate domains
     output:
-        boolean : whether the url has obfuscation or not, 1: True it does have an obfsucation, 0: False it doesn't have an obfiscaton
+        boolean : whether the url has obfuscation or not, 1: True it does have an obfuscation, 0: False it doesn't have an obfuscation
     '''
-
-    decoded_url = urllib.parse.unquote(url) # buat ngecek si urlnya ada pen-encode-an
+    decoded_url = urllib.parse.unquote(url)
     if decoded_url != url:
         return True
-    
-    normalized_url = unidecode(url)
 
+    normalized_url = unidecode(url)
     if normalized_url != url:
         return True
-    
+
     if len(url) > 100:
         return True
-    
-    if re.search(r'\b[a-fA-F0-9]{32}\b|\b[a-fA-F0-9]{40}\b', url): # pake hashing atau ngga
+
+    if re.search(r'\b[a-fA-F0-9]{32}\b|\b[a-fA-F0-9]{40}\b', url):
         return True
-    
+
     if count_subdomains(url) > 3:
         return True
-    
 
-    # Algorithm to find a major legit domain based on the tld counts
-    legit_domains = []
-    for tld in df['TLD'].unique():
-        count_0, count_1 = get_tld_counts(df,tld)
-        if count_0 > count_1:
-            domains_tld = df[df['TLD'] == tld]['Domain'].tolist()
-            legit_domains.extend(domains_tld)
     domain = extract_domain(url)
     if domain in legit_domains:
         return False
     return True
+
+def count_subdomains(url):
+    '''
+    Function to count the number of subdomains in a URL
+    Args:
+        url (str): The URL to evaluate
+    Returns:
+        int: The count of subdomains
+    '''
+    
+    domain = urlparse(url).hostname  
+    if domain:
+        parts = domain.split('.')
+        
+        return max(0, len(parts) - 2)
+    return 0
 
 def count_subdomains(url):
     '''
