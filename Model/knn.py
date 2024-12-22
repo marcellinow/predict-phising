a3e7dc33f4f8513ac7933bde82604b7b0771ec58
marcellinow <marcellino.gabriel12@gmail.com>
2024-12-22 17:39:34 +0700

    progress naive bayes scratches

--- Model/knn.py
+++ Model/knn.py
@@ -16,15 +16,10 @@ class KNN:
 
     def _predict(self, x):
         # Compute distances between x and all examples in the training set
-        distances, k_indices = self.kd_tree.query(x, k=self.k)
+        _, k_indices = self.kd_tree.query(x, k=self.k)
         # Sort by distance and return indices of the first k neighbors
-        k_indices = np.argsort(distances)[:self.k]
         # Extract the labels of the k nearest neighbor training samples
         k_nearest_labels = [self.y_train[i] for i in k_indices]
         # Return the most common class label
         most_common = np.bincount(k_nearest_labels).argmax()
         return most_common
-    
-
-def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2)**2))
