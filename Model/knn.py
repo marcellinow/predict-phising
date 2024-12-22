4c77b8c08b21664d7692284e6ff75c331743b5e2
marcellinow <marcellino.gabriel12@gmail.com>
2024-12-22 17:21:23 +0700

    mulai model from the scratch sama benerin struktur

--- Model/knn.py
+++ Model/knn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import Counter
+from scipy.spatial import KDTree
 
 class KNN:
     def __init__(self, k=3):
@@ -7,7 +7,8 @@ class KNN:
 
     def fit(self, X, y):
         self.X_train = X
-        self.y_train = y
+        self.y_train = np.array(y)
+        self.kd_tree = KDTree(X)
 
     def predict(self, X):
         predictions = [self._predict(x) for x in X]
@@ -15,11 +16,15 @@ class KNN:
 
     def _predict(self, x):
         # Compute distances between x and all examples in the training set
-        distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
+        distances, k_indices = self.kd_tree.query(x, k=self.k)
         # Sort by distance and return indices of the first k neighbors
         k_indices = np.argsort(distances)[:self.k]
         # Extract the labels of the k nearest neighbor training samples
         k_nearest_labels = [self.y_train[i] for i in k_indices]
         # Return the most common class label
-        most_common = Counter(k_nearest_labels).most_common(1)
-        return most_common[0][0]
+        most_common = np.bincount(k_nearest_labels).argmax()
+        return most_common
+    
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2)**2))
